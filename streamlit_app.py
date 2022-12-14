caf8b1ea6d7f8b849403707ea60f253cea5459ca
bwsnowflake <120493242+bwsnowflake@users.noreply.github.com>
2022-12-14 13:42:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('🍌🥭 Fruityvice Fruit Advice! 🥝🍇')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
